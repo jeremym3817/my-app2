83d8ac313a3a3868588b886850e0f4a73afb4fae
omegadc <dcleader99@gmail.com>
2024-02-24 03:35:27 -0500

    updated changes to java

--- backend_db/src/ConnectDB.java
+++ backend_db/src/ConnectDB.java
@@ -1,4 +1,3 @@
-package net.sqlitetutorial;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,7 +8,7 @@ import java.sql.SQLException;
  * @author sqlitetutorial.net
  */
 public class ConnectDB {
-     /**
+    /**
      * Connect to a sample database
      */
     public static void connect() {
@@ -21,9 +20,9 @@ public class ConnectDB {
             String url = "jdbc:sqlite:C:/Users/Admin/Desktop/Hackabeanpot hackathon/Hack-BeanPot-2024/tutorial.db";
             // create a connection to the database
             conn = DriverManager.getConnection(url);
-            
+
             System.out.println("Connection to SQLite has been established.");
-            
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -36,6 +35,7 @@ public class ConnectDB {
             }
         }
     }
+
     /**
      * @param args the command line arguments
      */
